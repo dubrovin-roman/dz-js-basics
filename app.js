1fd2578274a7b0dd5c3aaca7340f1a23a12e349b
Roman D. <romario.skfo@outlook.com>
2026-02-22 16:09:22 +0300

    done dz functions

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const hasLicence = true;
-const age = 18;
-const isDrunk = false;
+function convertCurrency(amountOfFunds, currencyOfFunds, targetCurrency) {
+    if (typeof amountOfFunds !== 'number' 
+        || amountOfFunds < 0 
+        || typeof currencyOfFunds !== 'string' 
+        || typeof targetCurrency !== 'string') {
+        return undefined;
+    }
 
-const result = hasLicence ? age >= 18 ? !isDrunk ? 'может' : 'не может' : 'не может' : 'не может';
-
-console.log(result);
+    const RUB_TO_USD = 76.7519;
+    switch (currencyOfFunds) {
+        case 'руб':
+            if (targetCurrency === '$') {
+                return amountOfFunds / RUB_TO_USD;
+            } else {
+                return null;
+            }
+        case '$':
+            if (targetCurrency === 'руб') {
+                return amountOfFunds * RUB_TO_USD;
+            } else {
+                return null;
+            }
+        default:
+            return null;
+    }
+}
